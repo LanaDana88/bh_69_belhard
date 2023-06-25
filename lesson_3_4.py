3f1e19cb91d83ef9a897a8ec3e548087a9c4954d
kama <kama@gmail.com>
2023-06-26 00:51:41 +0300

    //////

--- lesson_3_4.py
+++ lesson_3_4.py
@@ -5,3 +5,4 @@ number1 = int(input('enter number 1: '))
 number2 = int(input('enter number 2: '))
 number3 = int(input('enter number 3: '))
 print((number1 > 0) + (number2 > 0) + (number3 > 0), end='положительные, остальные отрицательные')
+
