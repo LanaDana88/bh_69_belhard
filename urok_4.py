03799c0caba017314231a9eda65b19704c31ef32
kama <kama@gmail.com>
2023-06-28 01:19:11 +0300

    ///

---
+++ urok_4.py
@@ -0,0 +1,89 @@
+# numbers = [1, 2, 3, 4, 5, 8, 10]
+# print(numbers)
+
+# colors = list(('красный', 'green', 'blue'))
+# print(colors)
+#
+# words = ['apple', 'box', 'milke', 'like']
+# uppercase_words = [word.upper() for word in words]
+# print(uppercase_words)
+
+# some_list = [2, 5, 7, 8, 10, 5]
+# del some_list[1:3:1]
+# print(some_list)
+
+# elem = some_list.remove(10)
+# elem = some_list.pop(2)
+# print(some_list)
+# print(elem)
+
+# search1 = 8 in some_list
+# search2 = 3 in some_list
+# print(search1)
+# print(search2)
+
+# some_list.append(15)
+# print(some_list)
+
+# some_list.insert(1, 20)
+# print(some_list)
+
+# words = 'Ekaterina, Alena'
+# some_list.extend(words)
+# print(some_list)
+
+# some_list.index(7)
+# print(some_list.index(10))
+
+# some_list.count(5)
+# some_list.count(11)
+# print(some_list.count(5))
+# print(some_list.count(11)) # если элемента нет, то выодит 0
+
+# some_list = [2, 5, 7, 8, 10, 5]
+# sorted(some_list)
+# sorted(some_list, reverse = True)
+# some_list.sort()
+# some_list.sort(reverse = True)
+# print(some_list)
+
+# some_list.reverse()
+# print(some_list)
+
+# some_list1 = [2, 5, 7, 8, 10, 5]
+# some_list1.clear()
+# print(some_list1)
+
+# some_list1.copy()
+# print(some_list1)
+
+# tuple_1 = ()
+# # print(tuple_1)
+# tuole_2 = tuple()
+# print(tuple)
+
+# set{1, 5, 7, 8, 10}
+
+# number = [ ]
+# n = 2
+# list_number = [2 ** number for number in range(1, n+1)]
+#
+# # print(list_number)
+# from collections import Counter
+#
+# letter_counter = Counter(input().split())
+# text = input({})
+# dict = {i: text.count(i) for i in text}
+# print(dict)
+
+# list = [2, 5, 8, 9, 10, 15, 48, 25, 18, 0]
+# my_list = {}
+# for items in list:
+#     my_list[items] = my_list.get(items, 0) + 1
+#
+#         print(my_list.items())
+n= 1
+my_dict = {}
+for items in my_dict:
+    my_dict[items] = my_dict.get([i for i in range(1, n+1)])
+    print(my_dict)
