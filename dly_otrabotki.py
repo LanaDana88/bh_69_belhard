8f479fe9017c4e285dcc2789aea4fb331ab74c9b
kama <kama@gmail.com>
2023-07-01 18:41:37 +0300

    //////

---
+++ dly_otrabotki.py
@@ -0,0 +1,68 @@
+# numbers = int(input('enter number: '))
+# if numbers % 2 == 0:
+#     print('вы ввели', numbers, 'четное число')
+# else:
+#     print('вы ввели', numbers, 'не четное число')
+#
+#
+# name = input('enter name: ')
+# age = int(input('enter age'))
+# privet = f"Hello {name} your age is {age}. Good luck!!!"
+# print(privet)
+#
+# year = int(input('enter year'))
+# if year % 4 == 0:
+#     print('этот год', year, 'является высокосным')
+# else:
+#     print('этот год', year, 'не является высокосным')
+
+# a = int(input('enter storona 1: '))
+# b = int(input('enter storona 2: '))
+# c = int(input('enter storona 3: '))
+# if a == b == c:
+#     print('тругольник равносторонний')
+# elif a == b or b == c or a == c:
+#     print('тругольник равнобедренный')
+# else:
+#     print('тругольник разносторонный')
+#
+# a_osnovanie = int(input('enter dl osnovaniya: '))
+# b_dlina = int(input('enter dlinu: '))
+# ploshad = (a_osnovanie * b_dlina) * 0.5
+# print(ploshad)
+
+# radius = int(input('enter radius okruzhosti: '))
+# diametr = 2 ** radius
+# pi = 3.14
+# ploshad = pi * (radius^2)
+# print('диаметр вашего круга', diametr, 'площать вашего круга', ploshad)
+
+# num1 = int(input('enter num1: '))
+# num2 = int(input('enter num2: '))
+# num3 = int(input('enter num3: '))
+# if num1 < num2 < num3:
+#     print('наименьшее число', num1)
+# elif num2 < num1 < num3:
+#     print('наименьшее число', num2)
+# else:
+#     print('наименьшее число', num3)
+
+# name = input('enter name: ')
+# print(name[::-1])
+#
+# stroka = input('enter stroku texta: ')
+# print(len(stroka))
+
+# num1 = int(input('enter num1: '))
+# num2 = int(input('enter num2: '))
+# sum = num1 * num2
+# print('произведение числ равно ', sum)
+
+# year = int(input('enter year of both: '))
+# date_today = int(input('enter year to day: '))
+# data_rozd = date_today - year
+# print('ваш возраст ', data_rozd, "лет")
+
+
+
+
