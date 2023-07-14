b78824b292715060f84f5309be5420ca939d3073
kama <kama@gmail.com>
2023-07-14 16:01:50 +0300

    ////

--- lesson_5_2.py
+++ lesson_5_2.py
@@ -1,5 +1,18 @@
+# num1 = int(input('enter number: '))
+# znacenie = 'Ваше число отрицательне или равно 0' if num1 <= 0 else int(input('enter number: '))
+#
+#
+# print(znacenie)
+
 num1 = int(input('enter number: '))
-znacenie = 'Ваше число отрицательне или равно 0' if num1 <= 0 else int(input('enter number: '))
+znak = input('enter operator: ')
+num2 = int(input('enter number: '))
+if znak == '+':
+    print(num1 + num2)
+elif znak == '/':
+    print(num1 - num2)
+elif znak == '*':
+    print(num1 * num2)
+else: print(num1 - num2)
 
 
-print(znacenie)
