b89d365826f4f580aa8ea7f4b0f8f84d6923a614
kama <kama@gmail.com>
2023-07-22 10:11:35 +0300

    ////

--- urok_10.py
+++ urok_10.py
@@ -51,76 +51,53 @@ print(BASE_DIR)
 # a - если файл есть, то дозаписываем новую информацию. в конец
 # x - на создание, не удобен нужен try   exept
 
-with (
-    open(BASE_DIR / 'input.txt', 'r', encoding='utf-8') as file,
-    open('output.txt', 'w', encoding='utf-8') as file2
-):
-    print(file.read())
+# with (
+#     open(BASE_DIR / 'input.txt', 'r', encoding='utf-8') as file,
+#     open('output.txt', 'w', encoding='utf-8') as file2
+# ):
+#     print(file.read())
+#
+# print(file.closed)
 
-print(file.closed)
+# например напишем файл 10 строчек с разрывом в  hello \n
+# \n - разрыв строки
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# with open('output.txt', 'w', encoding='utf-8') as file:
+#     from time import sleep
+#     for _ in range(10):
+#         file.write('hello\n')
+#         file.flush()
+#         sleep(3)
 
 # TODO дан текстовый многострочный файл, в каждой строке написаны числа через пробел
 # необходимо найти сумму чисел в каждой строки и результаты записать в новый файл
 #
 
-# with (
-# #     open(BASE_DIR / 'input.txt', 'r', encoding='utf-8') as input_file,
-# #     open(BASE_DIR / 'output.txt', 'w', encoding='utf-8') as output_file
-# # ):
-# #     output_file.writelines(
-# #             f'{sum(int(number) for number in line.strip().split())}\n'
-# #             for line in input_file if line.strip()
-# #     )
-# #     # result = []
-# #     # for line in input_file:
-# #     #     line = line.strip().split()
-# #     #     line = [int(i) for i in line]
-# #     #     result.append(f'{sum(line)}\n')
-# #     # output_file.writelines(result)
-#
-#
-# # with open('output.txt', 'w', encoding='utf-8') as file:
-# #     line = '*' * 100 + '\n'
-# #     while True:
-# #         file.write(line)
-#
+with (
+    open(BASE_DIR / 'input.txt', 'r', encoding='utf-8') as input_file,
+    open(BASE_DIR / 'output.txt', 'w', encoding='utf-8') as output_file
+ ):
+    output_file.writelines(
+            f'{sum(int(number) for number in line.strip().split())}\n'
+            for line in input_file if line.strip()
+    )
 
+# result = []
+# for line in input_file:
+#     line = line.strip().split()
+#     line = [int(i) for i in line]
+#     result.append(f'{sum(line)}\n')
+# output_file.writelines(result)
+#
+#
+# with open('output.txt', 'w', encoding='utf-8') as file:
+#       line = '*' * 7 + '\n'
+#       while True:
+#          file.write(line)
+#             break
+#
+# with open('output.txt', 'w', encoding='utf-8') as file:
+      #File.wriete('') # почистить файл, удалить все, т.е перезаписали
 
 
 
